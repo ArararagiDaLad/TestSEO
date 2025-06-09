4bfe26c188730cc36b124c6e7b9e18521baba0ed
ArararagiDaLad <hardinarthandsyach@gmail.com>
2025-06-09 15:00:11 +0700

    aa8

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: "Steel Dance",
     description:
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
+    url: "https://arthandcom.site/",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
